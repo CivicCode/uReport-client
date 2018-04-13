f0f56828ba605248ab0e015330cd61047ce4f418
Dana Palazzo <dana@fretless.com>
2018-04-12 20:53:22 -0400

    Form component renders name of selected service and simple form

--- src/components/FormContainer/index.js
+++ src/components/FormContainer/index.js
@@ -1,11 +1,72 @@
 import React from 'react'
+import { graphql } from 'react-apollo'
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
-const Form  = () => {
-  return(
-    <div>
-      Hello.
-    </div>
-  )
-}
+import './index.css';
+
+const Form = ({data, match}) => (
+  <Query
+    query={gql`
+      {
+        serviceList {
+          service_code
+          service_name
+          description
+          metadata
+          type
+          keywords
+          group
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+
+      const groupedByGroup = data.serviceList.reduce(
+        (acc, cv) => {
+          acc[cv.group] = acc[cv.group] || [];
+          acc[cv.group].push(cv);
+          return acc;
+        }, {});
+
+      return (
+        
+        <div className="Form">
+          <h1 className="headline">Report {(match.url).replace('/form/', '')}</h1>
+          <form action="#">
+            <label>
+              First Name:
+              <input type="text" name="firstName" placeholder="First Name" />
+            </label>
+            <br />
+            <label>
+              Last Name:
+              <input type="text" name="lastName" placeholder="Last Name" />
+            </label>
+            <br />
+            <label>
+              Email:
+              <input type="text" name="email" placeholder="jane@example.com" />
+            </label>
+            <br />
+            <label>
+              Pick the reason for your report:
+              <select>
+                <option value="happy">happy</option>
+                <option value="sad">sad</option>
+                <option value="99problems">99problems</option>
+                <option value="justBecause">just because</option>
+              </select>
+            </label>
+          </form>
+        </div>
+      );
+    }}
+  </Query>
+);
 
 export default Form;
