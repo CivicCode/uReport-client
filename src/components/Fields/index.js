fa1e9767954976288ac81f22f959cf8525c26ff3
Matt Batman <batman.matt@gmail.com>
2018-04-15 17:55:14 -0400

    Don't break out separate field components to avoid having to pipe state

--- src/components/Fields/index.js
+++
@@ -1,4 +0,0 @@
-export { default as DropDown } from './DropDown';
-export { default as Input } from './Input';
-export { default as Media } from './Media';
-export { default as TextArea } from './TextArea';
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,17 +1,113 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Field from '../Field';
+class Form extends Component {
+  constructor(props) {
+    super(props);
 
-const Form = ({name, code, fields}) => (
-  <div className="Form">
-    <h1 className="headline">Report {name}</h1>
-    <form action="#">
-    {
-      fields.map(field => <Field key={field.code} field={field} />)
+    this.state = {};
+
+    this.getField = this.getField.bind(this);
+    this.handleDropDownChange = this.handleDropDownChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleDropDownChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state);
+    event.preventDefault();
+  }
+
+  getField(field) {
+    const { code, datatype, description, values } = field;
+    switch (datatype) {
+      case 'string':
+      case 'number':
+      case 'datetime':
+        return (
+          <div className="Input" key={code}>
+            <label>
+              {description}:
+            </label>
+            <input
+              type="text"
+              name={code}
+              value={this.state.value}
+              onChange={this.handleInputChange}
+            />
+          </div>
+        );
+      case 'text':
+        return (
+          <div className="TextArea" key={code}>
+            <label>
+              {description}:
+            </label>
+            <textarea
+              type="text"
+              name={code}
+              value={this.state.value}
+              onChange={this.handleInputChange}
+            />
+          </div>
+        );
+      case 'media':
+        return (
+          <div className="Media" key={code}>
+            <label>
+              {description}:
+            </label>
+            <input type="file" name={code} />
+          </div>
+        );
+      case 'singlevaluelist':
+        return (
+          <div className="DropDown" key={code}>
+            <label>
+              {description}:
+            </label>
+            <select value={this.state.value} onChange={this.handleDropDownChange}>
+              {values.map(
+                value => (
+                  <option value={value.key}>{value.name}</option>
+                )
+              )}
+            </select>
+          </div>
+        );
+      default:
+        return null;
     }
-    <input type="submit" value="Submit" />
-    </form>
-  </div>
-);
+  }
+
+  render() {
+    const { name, code, fields } = this.props;
+
+    return (
+      <div className="Form">
+        <h1 className="headline">Report {name}</h1>
+        <form onSubmit={this.handleSubmit}>
+        {
+          fields.map(field => this.getField(field))
+        }
+        <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Form;
